11513a9866b6a41717e129bea10481340da11736
Areeb Ahmad Siddiqui <87814983+AreebAhmadSiddiqui@users.noreply.github.com>
2023-04-05 00:15:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MoviesPage from './components/MoviesPage';
+import TvPage from './components/TvPage'
+import DetailsPage from './components/DetailsPage'
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage/>} exact/>
+          <Route path='/movie/:id' element={<DetailsPage/>} />
+          <Route path='/tv/:id' element={<DetailsPage/>} />
+          <Route path='/movies' element={<MoviesPage/>} exact/>
+          <Route path='/tv' element={<TvPage/>} exact/>
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
